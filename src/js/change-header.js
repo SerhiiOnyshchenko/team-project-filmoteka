2ef6580ff6620164e8135670c2b7e61cff4d9a42
IlliaBekarevych <illiabekarevych@gmail.com>
2022-06-11 20:04:01 +0300

    FT-03

---
+++ src/js/change-header.js
@@ -0,0 +1,21 @@
+import refs from './refs';
+
+refs.logo.addEventListener('click', openHomePage);
+refs.btnHome.addEventListener('click', openHomePage);
+refs.btnMyLibrary.addEventListener('click', openMyLibrary);
+
+function openHomePage() {
+   refs.libraryBtns.classList.add('display-none');
+   refs.headerInput.classList.remove('display-none');
+   refs.btnMyLibrary.classList.remove('current');
+   refs.btnHome.classList.add('current');
+   refs.header.classList.remove('library__background');
+}
+
+function openMyLibrary() {
+   refs.header.classList.add('library__background');
+   refs.libraryBtns.classList.remove('display-none');
+   refs.headerInput.classList.add('display-none');
+   refs.btnMyLibrary.classList.add('current');
+   refs.btnHome.classList.remove('current');
+}
