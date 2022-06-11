0e561cbacd783cb97ed39402cac359ee1920a1f4
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-06-11 15:57:28 +0300

    FT-22: Register Login forms on desktop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 
 import './js/modal';
+import './js/registerLoginForm';
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,11 @@ const refs = {
    modal: document.querySelector('.modal-content'),
    backdrop: document.querySelector('.backdrop'),
    btnClose: document.querySelector('.btn-close'),
+
+   // register lgi form
+   signUpButton: document.getElementById('signUp'),
+   signInButton: document.getElementById('signIn'),
+   container: document.getElementById('container'),
 };
 
 export default refs;
