7a45a8796975fb1aef35d2ef0a75859c9a1e7140
IlliaBekarevych <illiabekarevych@gmail.com>
2022-06-11 19:42:53 +0300

    FT-02 change-header

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,12 @@ const refs = {
    modal: document.querySelector('.modal-content'),
    backdrop: document.querySelector('.backdrop'),
    btnClose: document.querySelector('.btn-close'),
+   logo: document.querySelector('.header__logo'),
+   btnHome: document.querySelector('.home__js'),
+   btnMyLibrary: document.querySelector('.my-library__js'),
+   header: document.querySelector('.header'),
+   libraryBtns: document.querySelector('.header__btns-container'),
+   headerInput: document.querySelector('.header__form-container')
 };
 
 export default refs;
