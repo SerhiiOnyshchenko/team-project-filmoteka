d636628ea967eae3d400d730f0c96ec90190a165
IlliaBekarevych <illiabekarevych@gmail.com>
2022-06-11 19:51:31 +0300

    FT-02 update

--- src/js/change-header.js
+++ src/js/change-header.js
@@ -2,7 +2,6 @@ import refs from './refs';
 
 refs.logo.addEventListener('click', openHomePage);
 refs.btnHome.addEventListener('click', openHomePage);
-refs.btnMyLibrary.addEventListener('click', openMyLibrary);
 
 function openHomePage() {
    refs.libraryBtns.classList.add('display-none');
@@ -10,12 +9,4 @@ function openHomePage() {
    refs.btnMyLibrary.classList.remove('current');
    refs.btnHome.classList.add('current');
    refs.header.classList.remove('library__background');
-}
-
-function openMyLibrary() {
-   refs.header.classList.add('library__background');
-   refs.libraryBtns.classList.remove('display-none');
-   refs.headerInput.classList.add('display-none');
-   refs.btnMyLibrary.classList.add('current');
-   refs.btnHome.classList.remove('current');
 }
