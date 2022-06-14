c09a08cf770376faec7a060e591e60a5686cb06f
LadyNenAdams <lovepaolabracho@gmail.com>
2022-06-14 16:49:15 +0300

    FT-10
    updated URL

--- src/js/services/movies-api.js
+++ src/js/services/movies-api.js
@@ -13,6 +13,7 @@ async function fetchWithErrorHandling(url = '', config = {}) {
 
 // FT-10
 export async function fetchFilmsByName(queryStr, page=1) {
-    const url = `${BASE_URL}search/movie?api_key=${MY_KEY}&query=${queryStr}`;
+    const url = `${BASE_URL}search/movie?api_key=${MY_KEY}&language=en-US&query=${queryStr}&page=${page}&include_adult=false` ;
+    // 
     return fetchWithErrorHandling(url)
 }
