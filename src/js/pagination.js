bfb7cd574f78ce4ac5054fd1bf5038c69afb37cb
Imavin08 <92112933+Imavin08@users.noreply.github.com>
2022-06-15 17:54:56 +0200

    add feature FT-08

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,10 +1,13 @@
 import refs from './refs';
+import { searchTrendFilms } from './searchTrendFilms';
+import { searchFilmByName } from './searchFilmByName';
 
 const iconArrowRight = '&#129130';
 const iconArrowLeft = '&#129128';
 let emptyrArray = [];
+let totalPages = 6;
+let clickPages = 1;
 
-const totalPages = 6;
 function renderBtn(total) {
    renderArrow(total, iconArrowLeft);
    for (let i = 1; i <= total; i += 1) {
@@ -19,7 +22,7 @@ function renderBtn(total) {
    refs.paginationList.innerHTML = '';
    refs.paginationList.insertAdjacentHTML('afterbegin', readyArray);
    const allBtn = document.querySelectorAll('.pagination__btn');
-   addCurrentFirstPage(allBtn);
+   addCurrentFirstPage(allBtn, clickPages);
 }
 renderBtn(totalPages);
 
@@ -29,13 +32,54 @@ function onBtnClick(e) {
    const currentBtn = e.target;
    if (currentBtn.nodeName === 'BUTTON' && currentBtn.dataset.id) {
       removeClassList();
-      console.log(currentBtn.dataset.id);
+      clickPages = Number(currentBtn.dataset.id);
       currentBtn.classList.add('pagination__btn--current');
+      removClasslistArrow();
+   } else {
+      onArrowClick(currentBtn);
+   }
+
+   if (whichTypeMovieSearch === 'trend') {
+      searchTrendFilms(clickPages);
+   } else if (whichTypeMovieSearch === 'search') {
+      searchFilmByName(searchText, clickPages);
    }
 }
 
-function addCurrentFirstPage(arrayBtn) {
-   arrayBtn[0].classList.add('pagination__btn--current');
+function onArrowClick(button) {
+   if (
+      button.nodeName === 'BUTTON' &&
+      button.className === 'pagination__btn--right js-pagination__btn--right'
+   ) {
+      clickPages += 1;
+      removClasslistArrow();
+      removeClassList();
+      addCurrentFromId();
+   } else if (
+      button.nodeName === 'BUTTON' &&
+      button.className === 'pagination__btn--left js-pagination__btn--left'
+   ) {
+      clickPages -= 1;
+      removClasslistArrow();
+      removeClassList();
+      addCurrentFromId();
+   }
+}
+
+function addCurrentFirstPage(arrayBtn, pageNumber) {
+   arrayBtn[pageNumber - 1].classList.add('pagination__btn--current');
+}
+
+function renderArrow(number, arrow) {
+   if (number > 5 && arrow === iconArrowRight) {
+      emptyrArray.push(
+         `<li class="pagination__item--right"><button class="pagination__btn--right js-pagination__btn--right">${arrow}</button></li>`
+      );
+   } else if (number > 5 && arrow === iconArrowLeft) {
+      emptyrArray.push(
+         `<li class="pagination__item--left pagination__hidden"><button class="pagination__btn--left js-pagination__btn--left">${arrow}</button></li>`
+      );
+   }
 }
 
 function removeClassList() {
@@ -45,14 +89,24 @@ function removeClassList() {
    }
 }
 
-function renderArrow(number, arrow) {
-   if (number > 5 && arrow === iconArrowRight) {
-      emptyrArray.push(
-         `<li class=" pagination__item--right"><button class="pagination__btn--right">${arrow}</button></li>`
-      );
-   } else if (number > 5 && arrow === iconArrowLeft) {
-      emptyrArray.push(
-         `<li class=" pagination__item--left"><button class="pagination__btn--left">${arrow}</button></li>`
-      );
+function removClasslistArrow() {
+   const arrowLeft = document.querySelector('.pagination__item--left');
+   const arrowRight = document.querySelector('.pagination__item--right');
+
+   if (clickPages < 2) {
+      arrowLeft.classList.add('pagination__hidden');
+   } else if (clickPages > 1) {
+      arrowLeft.classList.remove('pagination__hidden');
    }
+
+   if (clickPages === totalPages) {
+      arrowRight.classList.add('pagination__hidden');
+   } else if (clickPages < totalPages) {
+      arrowRight.classList.remove('pagination__hidden');
+   }
+}
+
+function addCurrentFromId() {
+   let idBtn = document.querySelector(`[data-id="${clickPages}"]`);
+   idBtn.classList.add('pagination__btn--current');
 }
