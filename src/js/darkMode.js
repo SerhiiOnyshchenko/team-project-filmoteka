aececdce06cc8fac253a77e64116c13e0e1a8739
LadyNenAdams <lovepaolabracho@gmail.com>
2022-06-19 21:49:45 +0300

    FT-24: localstorage for dark mode

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -2,10 +2,17 @@ const modeText = document.querySelector( ".change" );
 modeText.addEventListener("click", function() {
     if( document.body.classList.contains('dark')) {
       document.body.classList.remove('dark');
+      localStorage.setItem("mode", !"dark");
         modeText.innerHTML = "OFF";
     } else {
       document.body.classList.add('dark');
+      localStorage.setItem("mode", "dark");
       modeText.innerHTML = "ON";
     }
+
 });
+if (localStorage.getItem("mode") === "dark") {
+  document.body.classList.add('dark');
+  modeText.innerHTML = "OFF";
+}
 
