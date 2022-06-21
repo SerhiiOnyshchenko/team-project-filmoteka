c506fc8d8c168b5fdaea236e2d37d5f14edd4b88
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-06-21 12:27:33 +0300

    fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -13,7 +13,7 @@ let clickPage = 1;
 function pushInArray() {
    if (clickPage > 1) {
       emptyArray.push(
-         `<li class="pagination__item--left"><button class="pagination__btn--left js-pagination__btn--left">&#129128;</button></li>`
+         `<li class="pagination__item--left"><button class="pagination__btn--left js-pagination__btn--left"></button></li>`
       );
    }
    if (window.innerWidth < 768) {
@@ -115,7 +115,7 @@ function pushInArray() {
    }
    if (clickPage < totalPages) {
       emptyArray.push(
-         `<li class="pagination__item--right"><button class="pagination__btn--right js-pagination__btn--right">&#129130;</button></li>`
+         `<li class="pagination__item--right"><button class="pagination__btn--right js-pagination__btn--right"></button></li>`
       );
    }
 }
